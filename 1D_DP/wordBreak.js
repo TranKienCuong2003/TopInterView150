e231e952016542f30e113bb09753d99f8577540f
TranKienCuong2003 <trankiencuong30072003@gmail.com>
2025-01-08 04:40:21 +0700

    Add wordBreak.js file

---
+++ 1D_DP/wordBreak.js
@@ -0,0 +1,21 @@
+var wordBreak = function(s, wordDict) {
+    const wordSet = new Set(wordDict);
+    const dp = Array(s.length + 1).fill(false);
+    dp[0] = true;
+    
+    for (let i = 1; i <= s.length; i++) {
+        for (let j = 0; j < i; j++) {
+            if (dp[j] && wordSet.has(s.slice(j, i))) {
+                dp[i] = true;
+                break;
+            }
+        }
+    }
+    
+    return dp[s.length];
+};
+
+// Example
+console.log(wordBreak("leetcode", ["leet", "code"]));
+console.log(wordBreak("applepenapple", ["apple", "pen"]));
+console.log(wordBreak("catsandog", ["cats", "dog", "sand", "and", "cat"]));
