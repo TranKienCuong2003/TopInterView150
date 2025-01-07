df3e7e426e5cc15fcea18afe5c6d08f347896277
TranKienCuong2003 <trankiencuong30072003@gmail.com>
2025-01-08 04:42:09 +0700

    Add bestTimeToBuyAndSellStock.js file

---
+++ Array_String/bestTimeToBuyAndSellStock.js
@@ -0,0 +1,21 @@
+function maxProfit(prices) {
+    let minPrice = Infinity;
+    let maxProfit = 0;
+
+    for (let price of prices) {
+        if (price < minPrice) {
+            minPrice = price;
+        } else if (price - minPrice > maxProfit) {
+            maxProfit = price - minPrice;
+        }
+    }
+
+    return maxProfit;
+}
+
+// Example:
+let prices1 = [7, 1, 5, 3, 6, 4];
+console.log(maxProfit(prices1));
+
+let prices2 = [7, 6, 4, 3, 1];
+console.log(maxProfit(prices2));
