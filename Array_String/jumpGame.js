00e16ae6d1483ea95f2209592c08929e6aaea492
TranKienCuong2003 <trankiencuong30072003@gmail.com>
2025-01-08 04:45:48 +0700

    Add jumpGame.js file

---
+++ Array_String/jumpGame.js
@@ -0,0 +1,19 @@
+function canJump(nums) {
+    let maxReach = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        if (i > maxReach) {
+            return false;
+        }
+        maxReach = Math.max(maxReach, i + nums[i]);
+    }
+
+    return true;
+}
+
+// Example:
+let nums1 = [2, 3, 1, 1, 4];
+console.log(canJump(nums1));
+
+let nums2 = [3, 2, 1, 0, 4];
+console.log(canJump(nums2));
