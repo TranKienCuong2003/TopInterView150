4316428282cf0e57e8a1d393455ba6b7ce76ba3e
TranKienCuong2003 <trankiencuong30072003@gmail.com>
2025-01-09 02:00:38 +0700

    Add groupAnagrams.js file

---
+++ Hashmap/groupAnagrams.js
@@ -0,0 +1,16 @@
+function groupAnagrams(strs) {
+    let map = new Map();
+    for (let str of strs) {
+        let key = str.split('').sort().join('');
+        if (!map.has(key)) {
+            map.set(key, []);
+        }
+        map.get(key).push(str);
+    }
+    return Array.from(map.values());
+}
+
+// Example
+console.log(groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]));
+console.log(groupAnagrams([""]));
+console.log(groupAnagrams(["a"]));
