a2665cecae6937a175ec8c3807c89166a0686644
TranKienCuong2003 <trankiencuong30072003@gmail.com>
2025-01-09 02:17:39 +0700

    Add palindromeNumber.js file

---
+++ Math/palindromeNumber.js
@@ -0,0 +1,21 @@
+function isPalindrome(x) {
+    if (x < 0) {
+        return false;
+    }
+    
+    let original = x;
+    let reversed = 0;
+    
+    while (x > 0) {
+        let digit = x % 10;
+        reversed = reversed * 10 + digit;
+        x = Math.floor(x / 10);
+    }
+    
+    return original === reversed;
+}
+
+// Example
+console.log(isPalindrome(121));
+console.log(isPalindrome(-121));
+console.log(isPalindrome(10));
